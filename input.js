c892aa7dd13897fdd06b82836d31a2eed02c6594
Adam McPhee <adkmcphee@gmail.com>
2023-04-12 20:59:49 +0000

    intial input commit

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -11,6 +14,19 @@ const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
   }
+  if (key === 'w') {
+    console.log("Move: up")
+  }
+  if (key === 'a') {
+    console.log("Move: left")
+  }
+  if (key === 's') {
+    console.log("Move: down")
+  }
+  if (key === 'd') {
+    console.log("Move: right")
+  }
+
 };
 handleUserInput();
 
